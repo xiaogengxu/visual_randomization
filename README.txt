a89ac58575f30c85b685a3ba02af8f44830b764e
xiaogengxu <xiaogengxu1989@gmail.com>
2021-12-07 20:18:25 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+The app has two spinning wheels.
+The first wheel is used to illustrate random draw for general population.
+It helps understand each outcome is drawn with an equal chance.
+The second wheel allows subjects to randomly draw an outcome themselves.
+The texts in the wheel can be adapted.
---
+++ settings.py
@@ -0,0 +1,50 @@
+from os import environ
+
+
+SESSION_CONFIGS = [
+    dict(
+        name='visual_random',
+        display_name='Visual randomization',
+        num_demo_participants=1,
+        app_sequence=['visual_random']
+    ),
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True
+
+ROOMS = [
+    dict(
+        name='econ101',
+        display_name='Econ 101 class',
+        participant_label_file='_rooms/econ101.txt',
+    ),
+    dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
+]
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """
+Here are some oTree games.
+"""
+
+
+SECRET_KEY = 'm+7!ljtc9pwxq_8wvaik1s-to!l!&5&0o@y=(b-5hr$9r&o)xx'
+
+INSTALLED_APPS = ['otree']
